2ad2b63326be6fbcd1f13a28343022f9e4b14c92
Robert Fischer <robert.fischer@k4connect.com>
2020-08-12 12:59:36 -0400

    Move tsconfig up a level

---
+++ tsconfig/genTsconfig.js
@@ -0,0 +1,69 @@
+"use strict";
+
+const fs = require("fs");
+const _ = require("lodash");
+
+
+const TSCONFIG_VERSIONS = {
+  node4: {
+    extends: "recommended",
+    configOptions: {
+      target: "ES3",
+      downlevelIteration: true,
+    },
+  },
+  node10: {
+    extends: "node10",
+  },
+  node12: {
+    extends: "node12",
+  },
+  rn: {
+    extends: "react-native",
+  },
+};
+
+const COMMON_CONFIG = {
+  typeAcquisition: {
+    enable: true,
+  },
+  configOptions: {
+    checkJs: true,
+    composite: true,
+    declaration: true,
+    declarationMap: true,
+    sourceMap: true,
+    inlineSources: true,
+    importHelpers: true,
+    isolatedModules: true,
+    allowSyntheticDefaultImports: true,
+    noImplicitReturns: true,
+    noUnusedLocals: true,
+    noUnusedParameters: true,
+    allowUnreachableCode: false,
+    importsNotUsedAsValues: "error",
+    newLine: "lf",
+    resolveJsonModule: true,
+    useDefineForClassFields: true,
+  },
+  "$schema": "https://json.schemastore.org/tsconfig",
+};
+
+_.forEach(
+  TSCONFIG_VERSIONS,
+  (versionCfg, versionName) => fs.writeFileSync(
+    `${versionName}.json`,
+    JSON.stringify(_.merge({},
+      COMMON_CONFIG,
+      versionCfg, {
+        display: versionName,
+        extends: _.isEmpty(versionCfg.extends) ? null : `@tsconfig/${versionCfg.extends}/tsconfig.json`,
+      },
+    ), null, "\t")
+  )
+);
+
+_.forEach(
+  _.keys(TSCONFIG_VERSIONS),
+  (versionName) => process.stdout.write(`${versionName}.json\n`)
+);
