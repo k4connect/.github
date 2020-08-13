2631819bcd7419482fd5b0537f9b94c381563afd
Robert Fischer <robert.fischer@k4connect.com>
2020-08-13 12:40:01 -0400

    Initial check-in of the Desktop Browserslist config

---
+++ browserslist/desktop/README.md
@@ -0,0 +1,6 @@
+Desktop Browserslist Configuration
+========================================
+
+This is the configuration that exports our supported environments for non-mobile, non-Node environments.
+
+See the `README.md` file that is up a directory for details about how to use this.
---
+++ browserslist/desktop/browserslist-config.js
@@ -0,0 +1,30 @@
+module.exports = {
+	production: [
+		"cover 99% in US",
+		"last 1 year",
+		"last 2 major versions and > 0.1% in US",
+		"Edge >= 12 and last 7 years",
+		"Firefox ESR",
+		"last 2 Chrome major versions",
+		"last 2 Firefox major versions",
+		"last 2 Safari major versions",
+		"last 2 Edge major versions",
+		"not last 5 IE major versions",
+		"not last 5 and_chr major versions",
+		"not last 5 and_ff major versions",
+		"not last 5 and_uc major versions",
+		"not last 5 ios_saf major versions",
+		"not last 5 op_mini major versions",
+		"not last 5 samsung major versions",
+		"not android <= 99999999999",
+		"not dead",
+	],
+
+	local: [
+		"last 2 Chrome versions",
+		"unreleased Chrome versions",
+		"last 2 Firefox versions",
+		"unreleased Firefox versions",
+	],
+
+};
