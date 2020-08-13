10f45639621c61daadabdc8971ab74757b48f1b2
Robert Fischer <robert.fischer@k4connect.com>
2020-08-13 12:45:32 -0400

    Initial check-in of the mobile Browserslist config

---
+++ browserslist/mobile/README.md
@@ -0,0 +1,6 @@
+Mobile Browserslist Configuration
+========================================
+
+This is the configuration that exports our supported environments for mobile environments.
+
+See the `README.md` file that is up a directory for details about how to use this.
---
+++ browserslist/mobile/browserslist-config.js
@@ -0,0 +1,25 @@
+module.exports = {
+	production: [
+		"cover 99% in US and > 0.1% in US",
+		"last 5 and_chr major versions and > 0.1% in US",
+		"last 5 and_ff major versions and > 0.1% in US",
+		"last 5 ios_saf major versions and > 0.1% in US",
+		'last 5 op_mini major versions and > 0.1% in US",
+		'last 5 samsung major versions and > 0.1% in US",
+		"last 5 android major versions and > 0.1% in US",
+		"not dead",
+	],
+	local: [
+		"last 2 Chrome versions",
+		"unreleased Chrome versions",
+		"last 2 Firefox versions",
+		"unreleased Firefox versions",
+		"last 2 and_chr major versions",
+		"last 2 and_ff major versions",
+		"last 2 ios_saf major versions",
+		"last 2 android major versions",
+		"last 2 op_mini major versions",
+		"last 2 samsung major versions",
+		"not dead",
+	],
+};
